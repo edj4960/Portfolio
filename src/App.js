88aa06512606a40e0340bd70951ea610a912bbee
edj4960 <evandj423@gmail.com>
2021-07-11 09:56:54 -0600

    Addition of animated intro.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
   return (
     <div className="app" style={{height: '100%'}}>
       <Intro />
+      <div className="content" style={{height: 3000}}>
+        Some other really important stuff that you should definitely read.
+      </div>
     </div>
   );
 }
--- src/sections/Intro.js
+++ src/sections/Intro.js
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Divider from '../components/Divider';
 
+import './Intro.scss'
+
 const Intro = () => {
+    const [offsetY, setOffsetY] = useState(0);
+
+    const handleScroll = () => setOffsetY(window.pageYOffset);
+
+    useEffect(() => {
+
+    }, []);
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
     return (
-        <div>
-            <h1 className="text-light text-xlg">Hi, I'm Evan</h1>
-            <Divider />
+        <div className="intro-pg d-flex w-100">
+            <div 
+                className="left-section w-50 h-100 text-right align-content-center"
+                style={{transform: `translateX(${-offsetY}px)`}}
+            >
+                <h1 className="text-xlg visible font-title mr-4">Evan</h1>
+                <h4 className="mr-2">
+                    Full-Stack React 
+                </h4>
+            </div>
+            <div 
+                className="right-section w-50"
+                style={{transform: `translateX(${offsetY}px)`}}
+            >
+                <h1 className="text-xlg visible font-title ml-4">Jones</h1>
+                <h4 className="ml-1">
+                    and Node developer
+                </h4>
+            </div>
         </div>
     )
 }
